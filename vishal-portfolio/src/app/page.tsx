6591f1b8b19f03ca84e7c34561c5675cb7c3c49a
V1SHAL421 <70663959+V1SHAL421@users.noreply.github.com>
2024-06-08 22:17:53 +0100

    feat: Add icons

--- vishal-portfolio/src/app/page.tsx
+++ vishal-portfolio/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -107,6 +109,15 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        <button type="button" onClick={() => router.push('/projects')}>
+          Experience
+        </button>
+        <button type="button" onClick={() => router.push('/projects')}>
+          Projects
+        </button>
+        <button type="button" onClick={() => router.push('/projects')}>
+          Contact
+        </button>
       </div>
     </main>
   );
---
+++ vishal-portfolio/src/components/icons.tsx
@@ -0,0 +1,42 @@
+type IconProps = React.HTMLAttributes<SVGElement>;
+interface IconPropSize {
+    width: string;
+    height: string;
+    quality: string;
+}
+import Image from "next/image";
+
+// The icons that can be used on the website
+export const Icons = {
+    amadeusLogoPng: (props: IconProps) => (
+        <Image
+            src="/amadeus_logo.png"
+            alt="Amadeus Logo"
+            width="100"
+            height="70"
+            quality="100"
+            priority={true}
+        />
+    ),
+    ibmLogoPng: (props: IconProps) => {
+        <Image
+            src="/ibm_logo.png"
+            alt="IBM Logo"
+            width="80"
+            height="80"
+            quality="100"
+            priority={true}
+        />
+    },
+    vishalPng: (props: IconProps) => {
+        <Image
+            src="/vishal.png"
+            alt="Vishal"
+            width="80"
+            height="80"
+            quality="100"
+            priority={true}
+            />
+    }    
+
+}
