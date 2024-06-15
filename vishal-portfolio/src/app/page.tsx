d810a0def2bfb7d8f7f243e2a9d7f33ff5bc4db7
V1SHAL421 <vishalmkumar3@gmail.com>
2024-06-15 21:26:36 +0100

    fix: Sort out tailwind issue

--- vishal-portfolio/src/app/page.tsx
+++ vishal-portfolio/src/app/page.tsx
@@ -12,25 +12,37 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div>
           <Image
-          src={vishal}
-          alt="Vishal"
-          width={200}
-          height={400}
-          quality={100}
-          placeholder="blur"
-          className="w-auto max-w-xs"
+            src={vishal}
+            alt="Vishal"
+            width={200}
+            height={400}
+            quality={100}
+            placeholder="blur"
+            className="w-auto max-w-xs rounded-full mx-auto"
           />
-          <button type="button" onClick={() => router.push('/experience')}>
+          <button
+            type="button"
+            onClick={() => router.push('/experience')}
+            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+          >
             Experience
           </button>
-          </div>
-          <div>
-          <button type="button" onClick={() => router.push('/myprojects')}>
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => router.push('/myprojects')}
+            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+          >
             Projects
           </button>
-          </div>
-          <div>
-          <button type="button" onClick={() => router.push('/contacts')}>
+        </div>
+        <div>
+          <button
+            type="button"
+            onClick={() => router.push('/contacts')}
+            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+          >
             Contacts
           </button>
         </div>
