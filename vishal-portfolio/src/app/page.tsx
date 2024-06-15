08991c716533976d783a8a84ef8c2e6d991737c0
V1SHAL421 <vishalmkumar3@gmail.com>
2024-06-15 22:02:11 +0100

    feat: Add title

--- vishal-portfolio/src/app/page.tsx
+++ vishal-portfolio/src/app/page.tsx
@@ -4,13 +4,34 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Icons } from "@/components/icons";
 import vishal from "@/../public/vishal.png"
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
 
 export default function Home() {
   const router = useRouter()
+  const words = [
+    {
+      text: "Welcome",
+      className: "text-white"
+    },
+    {
+      text: "to",
+      className: "text-white"
+    },
+    {
+      text: "my",
+      className: "text-white"
+    },
+    {
+      text: "Portfolio",
+      className: "text-purple-700"
+    }
+  ]
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div>
+      <div className="z-10 w-full flex-col max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <TypewriterEffect words={words} className="mb-20"/>
+        <div className="flex flex-row items-center">
           <Image
             src={vishal}
             alt="Vishal"
@@ -20,31 +41,32 @@ export default function Home() {
             placeholder="blur"
             className="w-auto max-w-xs rounded-full mx-auto"
           />
+          <div className="flex flex-col items-center px-4 py-4">
           <button
             type="button"
             onClick={() => router.push('/experience')}
-            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+            className="rounded mt-4 mb-4 bg-blue-700 hover:bg-red-300 text-white py-2 px-4 font-bold"
           >
             Experience
           </button>
-        </div>
-        <div>
+        
+        
           <button
             type="button"
             onClick={() => router.push('/myprojects')}
-            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+            className="rounded mt-4 mb-4 bg-blue-700 hover:bg-red-300 text-white py-2 px-4 font-bold"
           >
             Projects
           </button>
-        </div>
-        <div>
+
           <button
             type="button"
             onClick={() => router.push('/contacts')}
-            className="rounded mt-4 bg-blue-500 hover:bg-red-300 text-white py-2 px-4 font-bold"
+            className="rounded mt-4 mb-4 bg-blue-700 hover:bg-red-300 text-white py-2 px-4 font-bold"
           >
             Contacts
           </button>
+          </div>
         </div>
       </div>
     </main>
